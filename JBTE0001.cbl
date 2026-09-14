6ac10e0e1708450b1dbc3ab8d093a93129c4a2f9
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2026-09-14 15:07:21 +1000

    Update JBTE0001.cbl

--- JBTE0001.cbl
+++ JBTE0001.cbl
@@ -6,10 +6,16 @@
 
        data division.
        working-storage section.
+           01  ws-p1       PIC X(2).
+       local-storage section.
+           01 ls-p1        PIC X(2).
 
        procedure division.
            DISPLAY "JBTE0001 Version 1.0"
            DISPLAY "Remote:This is on the main branch"
+           DISPLAY "Enter a parameter (2 ch)"
+           ACCEPT ws-p1
+           DISPLAY "Parameter enteredx=" ws-p1
            goback
            .
 
