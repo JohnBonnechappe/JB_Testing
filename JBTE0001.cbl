022eb06ac135d9b2d60fbabca99f990a2dd19596
John Braddock <John.Braddock@ato.gov.au>
2026-09-21 15:16:13 +1000

    Jaxx up JBTE0001

--- JBTE0001.cbl
+++ JBTE0001.cbl
@@ -6,9 +6,17 @@
 
        data division.
        working-storage section.
+       01 WS-PARAM-1  PIC X(10).
+       01 WS-OUT-1    PIC X(12).
 
        procedure division.
            DISPLAY "JBTE0001 Version 1.0"
+           DISPLAY "Enter a code:"
+           Accept WS-PARAM-1
+           STRING WS-PARAM-1,"JB"
+             DELIMITED BY SIZE INTO WS-OUT-1
+
+           DISPLAY "Code Entered=" WS-OUT-1
            goback
            .
 
