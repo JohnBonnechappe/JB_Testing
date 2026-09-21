7a0b119d4876a9a85d97f3724b06dbd5a37be657
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2026-09-21 15:28:02 +1000

    Create JBTE0004.cbl

---
+++ JBTE0004.cbl
@@ -0,0 +1,15 @@
+       identification division.
+       program-id. JBTE0004.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+           DISPLAY "JBtest04 Version 1.0"
+           DISPLAY "Remote WIP2 copy"
+           goback.
+
+       end program JBTE0004.
