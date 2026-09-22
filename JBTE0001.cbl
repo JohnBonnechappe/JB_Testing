dc48ad0944a6afd28d0c4c004c5be95358dffa49
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2026-09-22 16:03:08 +1000

    Update JBTE0001.cbl
    make main 2.1 of TE0001

--- JBTE0001.cbl
+++ JBTE0001.cbl
@@ -12,7 +12,7 @@
 
        procedure division.
            DISPLAY "JBTE0001 Version 1.0"
-           DISPLAY "Remote:This is on the main branch V2"
+           DISPLAY "Remote:This is on the main branch V2.1"
            DISPLAY "Enter a parameter (2 ch)"
            ACCEPT ws-p1
            DISPLAY "Parameter enteredx=" ws-p1
