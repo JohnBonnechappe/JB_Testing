5b6c733296aa7e1e704d2583746cf4983607537d
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2026-09-22 15:09:39 +1000

    Update JBTE0001.cbl

--- JBTE0001.cbl
+++ JBTE0001.cbl
@@ -12,7 +12,7 @@
 
        procedure division.
            DISPLAY "JBTE0001 Version 1.0"
-           DISPLAY "Remote:This is on the main branch"
+           DISPLAY "Remote:This is on the wip branch"
            DISPLAY "Enter a parameter (2 ch)"
            ACCEPT ws-p1
            DISPLAY "Parameter enteredx=" ws-p1
