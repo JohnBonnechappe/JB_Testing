06181e4dc2ba4c9d35c41d80922c8c32e518b0a3
John Braddock <John.Braddock@ato.gov.au>
2026-09-23 11:33:48 +1000

    Mod in local main 23-sep

--- JBTE0003.cbl
+++ JBTE0003.cbl
@@ -11,7 +11,7 @@
 
        procedure division.
            DISPLAY "JBtest03 Version 1.0"
-           DISPLAY "Modified in Local"
+           DISPLAY "Modified in Local main branch 23-sep"
            DISPLAY "ENter a code"
            ACCEPT WS-PARAM-1
            DISPLAY "Code Entered=" WS-PARAM-1
