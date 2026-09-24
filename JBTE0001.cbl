24e4007ad698898e9dc1fa319567ab5002d7a010
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2026-09-24 14:43:56 +1000

    Update JBTE0001.cbl

--- JBTE0001.cbl
+++ JBTE0001.cbl
@@ -11,7 +11,7 @@
 
        procedure division.
            DISPLAY "JBTE0001 Version 1.0"
-           DISPLAY "This is on the WIP2 branch vers2.1a"
+           DISPLAY "This is on the WIP2 branch vers2.1aaaaaa"
            DISPLAY "Enter a code:"
            Accept WS-PARAM-1
            STRING WS-PARAM-1,"JB"
