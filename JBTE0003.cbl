62523463a5d17f55d3a4a8ca8adc2f5aed66e819
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2026-09-24 10:35:47 +1000

    Update JBTE0003.cbl
    testing branches

--- JBTE0003.cbl
+++ JBTE0003.cbl
@@ -9,6 +9,7 @@
 
        procedure division.
            DISPLAY "JBtest03 Version 1.0 changed in local WIP2"
+           DISPLAY "testing branches"
            goback.
 
        end program JBTE0003.
